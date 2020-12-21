13407ecb99abec78f1e33016076922d8f6faa959
Kevin williams permana <kevinity310@gmail.com>
2020-12-21 14:18:05 +0700

    Tipe data Character

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,3 +1,10 @@
 fun main(){
+// karakter terdiri satu huruf dan menggunakan tanda petik satu
+    var e: Char = 'E';
+    var k: Char = 'K';
+    var o: Char = 'O';
 
+    print(e)
+    print(k)
+    print(o)
 }
