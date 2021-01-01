33d13ab361877fc5a0b57215424b6b4fb974ed60
Kevin williams permana <kevinity310@gmail.com>
2021-01-01 13:40:13 +0700

    Belajar Operasi Perbadingan

---
+++ src/main/kotlin/OperasiPerbandingan.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a = 100
+    val b = 200
+
+    val result : Boolean = a < b
+    println(result)
+
+    println(100 >= 200)
+    println(100 <= 200)
+
+    println("a" == "a")
+    println("a" > "b")
+
+
+}
