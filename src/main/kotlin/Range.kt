52b872e46b5ec0bbccb25fb99505e64fe35ba16d
Kevin williams permana <kevinity310@gmail.com>
2021-01-01 12:42:32 +0700

    Tipe data Range.kt

--- src/main/kotlin/Range.kt
+++ src/main/kotlin/Range.kt
@@ -3,8 +3,8 @@ fun main(){
     val range = 1..100 step 2
     //val range = 100 downTo 1
 
-    println(range.count())
-    println(range.contains(50))
+    println(range.count()) // banyak data
+    println(range.contains(3)) // apakah data memiliki nilai data 2?
     println(range.contains(200))
     println(range.first)
     println(range.last)
