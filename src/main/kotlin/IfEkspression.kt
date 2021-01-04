147930d0db1911d971792ff63d72734518e55b43
Kevin williams permana <kevinity310@gmail.com>
2021-01-04 15:32:16 +0700

    Belajar ifExpression

---
+++ src/main/kotlin/IfEkspression.kt
@@ -0,0 +1,14 @@
+fun main() {
+     val nilai = 95
+
+    if (nilai > 90 ){
+        println("Excellent =)")
+    }else if (nilai > 75){
+        println("Good job :)")
+    }else if (nilai > 60 ){
+        println("Not Bad =|")
+    }else {
+        println("Please try again next year :(")
+    }
+
+}
