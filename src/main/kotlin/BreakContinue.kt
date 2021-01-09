d9954e983fc06082486a4b1124044c2d990fe4f9
Kevin williams <kevinity310@outlook.com>
2021-01-09 20:41:33 +0700

    Belajar Break & Continue

---
+++ src/main/kotlin/BreakContinue.kt
@@ -0,0 +1,23 @@
+fun main() {
+    // Break
+    var i = 0
+
+    while (true) {
+        println("Perulangan ke $i")
+        i++
+
+        if (i > 1000){
+            break
+        }
+    }
+
+    //Continue
+
+    for (i in 0..100){
+        if(i % 2 == 0){
+            continue  // angka genap tidak di print
+        }
+    println("Angka $i")
+    }
+
+}
