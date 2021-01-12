4ebc13fc13bb45271c6d5efec48ac5bfaee500ea
Kevin williams <kevinity310@outlook.com>
2021-01-12 22:23:10 +0700

    Belajar Unit Returning Data ( lupa sebelumnya menambahkan unitnya )

--- src/main/kotlin/UnitReturningData.kt
+++ src/main/kotlin/UnitReturningData.kt
@@ -1,4 +1,4 @@
-fun printHello(name : String? = null){
+fun printHello(name : String? = null) : Unit {  // secara default Unit sudah ada,
     if (name == null) {
         println("Hello Broku")
     }else {
