60d4aad08988715942d93e760abcb7de5f512b6d
Kevin williams <kevinwilliamsubk1@gmail.com>
2021-02-04 13:41:34 +0700

    Belajar Main Parameter

---
+++ src/main/kotlin/Package.kt
@@ -0,0 +1,8 @@
+import com.kevin.belajar.util.*
+
+fun main() {
+    sayHello("Kevin")
+    sayGoodbye("Wiliams")
+
+    //com.kevin.belajar.util.sayGoodbye("Williams")
+}
---
+++ src/main/kotlin/com/kevin/belajar/util/Stringutil.kt
@@ -0,0 +1,9 @@
+package com.kevin.belajar.util
+
+fun sayHello(name: String){
+    println("Hello $name")
+}
+
+fun sayGoodbye(name: String){
+    println("Good Bye $name")
+}
